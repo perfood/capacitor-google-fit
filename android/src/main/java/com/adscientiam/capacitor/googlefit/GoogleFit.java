4633e231def03af96684086a4768c986f3434556
Arnaud Drain <adrain@adscientiam.com>
2019-10-30 15:44:25 +0100

    Use android support versions

--- android/src/main/java/com/adscientiam/capacitor/googlefit/GoogleFit.java
+++ android/src/main/java/com/adscientiam/capacitor/googlefit/GoogleFit.java
@@ -2,7 +2,7 @@ package com.adscientiam.capacitor.googlefit;
 
 import android.content.Intent;
 
-import androidx.annotation.NonNull;
+import android.support.annotation.NonNull;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
